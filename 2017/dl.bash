966a1dbde6cb8ae79fd2ac801a3b41fcc616cb2b
Benoît Zugmeyer <bzugmeyer@gmail.com>
2017-12-01 13:58:53 +0100

    2017 - Day 1

---
+++ 2017/dl.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+dayname=$(printf day%02d $2)
+mkdir -p tests/$dayname
+curl "http://adventofcode.com/2017/day/$2/input" -H "Cookie: session=$1" > tests/$dayname/input
+touch tests/$dayname/output
+git add tests/$dayname/input tests/$dayname/output
---
+++ 2017/src/bin/day01.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let numbers: Vec<_> = input
+        .chars()
+        .flat_map(|ch| ch.to_digit(10))
+        .collect();
+
+    let mut part_1 = 0;
+    let mut part_2 = 0;
+    for (index, n) in numbers.iter().enumerate() {
+        if n == &numbers[(index + 1) % numbers.len()] {
+            part_1 += n;
+        }
+        if n == &numbers[(index + numbers.len() / 2) % numbers.len()] {
+            part_2 += n;
+        }
+    }
+
+    println!("Part 1: {:?}", part_1);
+    println!("Part 2: {:?}", part_2);
+}
