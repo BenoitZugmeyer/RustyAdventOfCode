0069d4e97074c47426fb4db40e28cc60827d6bcb
Benoît Zugmeyer <bzugmeyer@gmail.com>
2018-10-07 18:43:31 +0200

    fix dl.bash

--- 2017/dl.bash
+++ 2017/dl.bash
@@ -4,6 +4,6 @@ set -euo pipefail
 
 dayname=$(printf day%02d $2)
 mkdir -p tests/$dayname
-curl "http://adventofcode.com/2017/day/$2/input" -H "Cookie: session=$1" > tests/$dayname/input
+curl "https://adventofcode.com/2017/day/$2/input" -H "Cookie: session=$1" > tests/$dayname/input
 touch tests/$dayname/output
 git add tests/$dayname/input tests/$dayname/output
